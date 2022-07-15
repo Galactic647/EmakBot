2c023969381603397c19d6707273f16cc0939941
Galactic647 <44773161+Galactic647@users.noreply.github.com>
2022-07-15 23:52:05 +0800

    Update kick.py

--- cogs/kick.py
+++ cogs/kick.py
@@ -1,10 +1,11 @@
-from globals import ERROR_MESSAGE, DEVELOPMENT
+from globals import DEVELOPMENT
 from core import decorators, descriptions as desc, functions as func
 from core.logger import logger
 
 from discord.ext import commands
 import discord
 
+from typing import Union
 import asyncio
 
 
@@ -13,30 +14,25 @@ class Kick(commands.Cog):
         self.bot = bot
 
     @commands.command(brief=desc.KICK_BRIEF, description=desc.KICK)
-    @decorators.in_channels(has_user=True)
+    @decorators.in_channels(has_target=True)
     @decorators.self_check
-    async def kick(self, ctx, user: discord.Member, reason: str) -> None:
+    async def kick(self, ctx, target: Union[discord.Member, str], reason: str) -> None:
         def check(msg):
             return msg.author == ctx.author and msg.channel == ctx.channel and msg.content.lower() in ["y", "n"]
 
         try:
-            await ctx.channel.send(f'Apakah anda yakin ingin mengkick user {user}? [Y/N]')
+            await ctx.channel.send(f'Apakah anda yakin ingin mengkick user {target}? [Y/N]')
             reply = await self.bot.wait_for('message', check=check, timeout=15)
             if reply.content.lower() == 'y':
                 msg_id = func.generate_id()
-                embed = func.embedder('Kick', msg_id, ctx.author.mention, user, user.avatar_url, reason, str(), str())
+                embed = func.embedder('Kick', msg_id, ctx.author.mention, target, target.avatar_url,
+                                      reason, str(), str())
 
                 await ctx.channel.send(embed=embed)
                 if not DEVELOPMENT:
-                    await self.bot.kick(user, reason=reason)
-                    logger.info(f'User {ctx.author} just kick a user\n'
-                                f'User: {user}\n'
-                                f'Reason: {reason}')
+                    await self.bot.kick(target, reason=reason)
         except asyncio.TimeoutError:
             await ctx.channel.send('Command di cancel')
-        except Exception as e:
-            await ctx.channel.send(ERROR_MESSAGE.format(e))
-            logger.error(e)
 
 
 def setup(bot):
